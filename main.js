b8d80a4b140d0974ac544a0815d116e4da63090d
Joshua Bennett <legodude17@users.noreply.github.com>
2017-01-24 13:11:48 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 var l = localStorage, _ = document;
 
 var editor = {
-  create() {
+  create(type, theme) {
     var obj = Object.create(this);
     obj.elm = _.createElement('div');
     this.elm.appendChild(obj.elm);
+    obj.elm.classList.add('editor');
     obj.editor = ace.edit(elm);
+    obj.editor.getSession().setMode('ace/modes/' + type);
+    obj.editor.setTheme('ace/themes/' + theme);
+    this.editors.push(obj);
     return obj;
   },
-  
-  elm: _.getElementById('editors');
+  editors: [],
+  elm: _.getElementById('editors')
 };
