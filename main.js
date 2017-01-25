b4c84bc1c982e936ad8cecbd744cf41cfdcd8311
Joshua Bennett <legodude17@users.noreply.github.com>
2017-01-25 09:55:31 -0800

    Add add

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 var l = localStorage, _ = document;
 
-var editor = {
+var fs = (function (){
+  var obj = {};
+  BrowserFS.install(obj);
+  var lsfs = new BrowserFS.FileSystem.LocalStorage();
+  BrowserFS.initialize(lsfs);
+  return obj.require('fs');
+});
+
+var editors = {
   create(type, theme) {
     var obj = Object.create(this);
     obj.elm = _.createElement('div');
@@ -15,3 +23,17 @@ var editor = {
   editors: [],
   elm: _.getElementById('editors')
 };
+
+var extToType = {
+  'js': 'javascript',
+  'md': 'markdown',
+  'html': 'html',
+  'css': 'css'
+};
+
+function add() {
+  var fileName = prompt('Filename?');
+  var type = extToType(fileName.split('.').pop());
+  var editor = editors.create(type);
+  return new Promise((resolve, reject) => fs.writeFile(fileName, '', (err, res) => err ? reject(err) : resolve(res)));
+}
